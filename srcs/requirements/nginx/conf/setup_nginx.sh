ea86e8d1efc892aaecdc333aef5f39845bac3ed6
Paulo Cunha <pcunha00@hotmail.com>
2022-05-30 19:25:11 -0300

    refactor nginx dockerfile

--- srcs/requirements/nginx/conf/setup_nginx.sh
+++ srcs/requirements/nginx/conf/setup_nginx.sh
@@ -4,3 +4,7 @@
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 	-subj '/C=BR/ST=SP/L=SP/O=42SP/CN=pcunha' \
 	-keyout /etc/ssl/private/localhost.key -out /etc/ssl/certs/localhost.crt
+
+
+# Copy config file to correct directory
+cp ./conf/nginx.conf /etc/nginx/sites-available/default
